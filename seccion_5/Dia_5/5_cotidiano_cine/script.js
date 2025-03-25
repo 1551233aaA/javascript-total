d4049fa3c339cabbee109edf9ad52b7efdc76dc2
jpgonzalez <gonzalesjosepablo0@gmail.com>
2025-03-24 19:15:11 -0600

    cotidiano cine

--- seccion_5/Dia_5/5_cotidiano_cine/script.js
+++ seccion_5/Dia_5/5_cotidiano_cine/script.js
@@ -3,41 +3,40 @@ const genreButtons = document.querySelectorAll(".genre-button");
 
 genreButtons.forEach(button => {
     button.addEventListener("click", () => {
-        const age = parseInt(document.getElementById("age").value);
+        const edad = parseInt(document.getElementById("edad").value);
         const genre = button.getAttribute("data-genre");
 
-        if (isNaN(age) || age <= 0) {
+        if (isNaN(edad) || edad <= 0) {
             recommendationElement.textContent = "Por favor, introduce una edad válida.";
             return;
         }
 
         let movie = "";
-
         switch (genre) {
             case "Drama":
-                if (age < 13) movie = "Casablanca";
-                else if (age < 16) movie = "The Shawshank Redemption";
+                if (edad < 13) movie = "Casablanca";
+                else if (edad < 16) movie = "The Shawshank Redemption";
                 else movie = "Taxi Driver";
                 break;
             case "Comedia":
-                if (age < 13) movie = "Back to the Future";
-                else if (age < 16) movie = "Ferris Bueller's Day Off";
-                else movie = "The Big Lebowski";
+                if (edad < 13) movie = "Back to the Future";
+                else if (edad < 16) movie = "The Truman Show ";
+                else movie = "The Wolf of Wall Street ";
                 break;
             case "Musical":
-                if (age < 13) movie = "La La Land";
-                else if (age < 16) movie = "Les Misérables";
+                if (edad < 13) movie = "La La Land";
+                else if (edad < 16) movie = "Les Misérables";
                 else movie = "The Rocky Horror Picture Show";
                 break;
             case "Crimen":
-                if (age < 13) movie = "No hay opciones para esta edad.";
-                else if (age < 16) movie = "El Secreto de sus Ojos";
+                if (edad < 13) movie = "No hay opciones para esta edad.";
+                else if (edad < 16) movie = "El Secreto de sus Ojos";
                 else movie = "The Godfather";
                 break;
             default:
                 movie = "Género no válido.";
         }
-
+       
         recommendationElement.textContent = movie;
     });
 });
